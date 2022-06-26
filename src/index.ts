055203320f386eb5fe11d2c00d6e5428a62e2641
alWasayf <49132323+alWasayf@users.noreply.github.com>
2022-06-26 12:37:11 +0300

    app

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { server } from './server';
+
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
+
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	});
+
+
+/*import { server } from './server';
+
+server.listen({ port: 3000 }).catch((err) => {
+	server.log.error(err);
+	process.exit(1);
+});*/
+
